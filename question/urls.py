efb4af9323f515e561cef27e1b18b85ae7feb0c8
rathee95 <rathee95@gmail.com>
2016-12-14 17:21:48 +0530

    added JsonResponse for Question

--- question/urls.py
+++ question/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import all_questions 
+from .views import all_questions, get_questions 
 
 urlpatterns = [
     url(r'^all/$',  all_questions),
+    url(r'^(?P<id>[0-9]+)/$',get_questions )
 ]
--- question/views.py
+++ question/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render #renderig all file handling work 
 from .models import Question
 # Create your views here.
+from django.http import Http404 , JsonResponse
+	
 
 def all_questions(request):
 	context= {'q_list':Question.objects.all()}#the keys in dict will become variables in templates
 	return render(request,'question/index.html',context)
+
+def get_questions(request,id = None):
+	if not id:
+		raise Http404;
+	try:
+		q = Question.objects.get(id=id)
+	except Question.DoesNotExist:
+		raise Http404
+
+	data = {'id': q.id , 'title': q.title , 'text': q.text}
+	
+	return JsonResponse(data)
