884710a96dd68a335d12458990a23bdc039d3896
rathee95 <rathee95@gmail.com>
2016-12-14 19:03:14 +0530

    Post request for adding a question via a form added, csrf token handling added

--- question/urls.py
+++ question/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import all_questions, get_questions 
+from .views import all_questions, get_questions , show_question_add_form, save_question
 
 urlpatterns = [
     url(r'^all/$',  all_questions),
-    url(r'^(?P<id>[0-9]+)/$',get_questions )
+    url(r'^(?P<id>[0-9]+)/$',get_questions ),
+    url(r'^add/$',show_question_add_form ),
+    url(r'^save/$',save_question),
 ]
--- question/views.py
+++ question/views.py
@@ -3,16 +3,38 @@ from django.shortcuts import render, get_object_or_404
 from .models import Question
 # Create your views here.
 from django.http import Http404 , JsonResponse, HttpResponse
+from django.views.decorators.http import require_GET,require_POST, require_http_methods	
 from django.core import serializers	
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def all_questions(request):
 	context= {'q_list':Question.objects.all()}#the keys in dict will become variables in templates
 	return render(request,'question/index.html',context)
 
-def get_questions(request,id = None):
-	if not id:
-		raise Http404;
+@require_GET
+def show_question_add_form(request):
+ 	return render(request,'question/create_form.html')
 
+@require_POST
+def save_question(request):
+	title = request.POST.get('title','')
+	if not title:
+		raise Http404
+
+	q = Question.objects.create(title = title ,created_by = request.user)
+
+	print(request.POST)
+	return HttpResponse('ok')
+
+@require_GET
+@csrf_exempt	
+def get_questions(request,id = None):
+	# if request.method != 'GET':
+	# 	raise Http404
+	# this has been provided in a decorator
+	if not id:
+		raise Http404
 	# try:
 	# 	q = Question.objects.get(id=id)
 	# except Question.DoesNotExist:
