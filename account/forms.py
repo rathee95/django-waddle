79d19ec2e831fa5d061bcb2a01a267db5b40b372
rathee95 <rathee95@gmail.com>
2017-03-31 23:01:27 +0530

    updation to continue

--- account/forms.py
+++ account/forms.py
@@ -1,22 +1,19 @@
 from django import forms
 from account.models import MyUser
 from django.contrib.auth import authenticate
+
 class LoginForm(forms.Form):
 	username = forms.CharField(max_length = 25)
 	password = forms.CharField(widget = forms.PasswordInput)
-
 	#clean_ are called by itself on is_valid() call
 	#cleaned_data ----> dict key:value
 	#validation logic is shifted inside the form 
-
-
 	#this is field error  ----->  clean_fieldname
 	def clean_username(self):
 		username = self.cleaned_data['username']
 		if MyUser.objects.filter(username = username).count() != 1:
 			raise forms.ValidationError('Invalid Username')
 		return username
-
 	#this runs for multiple fields and their dependency .
 	#check username matches with password here 
 	def clean(self):
